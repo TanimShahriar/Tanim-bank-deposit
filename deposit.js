c9965d5ec4f08612b9a6f5c5ccf2acebb04efd12
TanimShahriar <ssmtsshr@gmail.com>
2023-08-15 18:15:54 +0600

    second commit

--- deposit.js
+++ deposit.js
@@ -3,13 +3,29 @@ document.getElementById('btnDeposit').addEventListener('click', function () {
   //step-2: get the deposit amount from deposit input field
   //for input field use .value to the input field
   const depositField = document.getElementById('depositField');
-  const depositAmount = depositField.value;
+  const newDepositAmountString = depositField.value;
+  const newDepositAmount = parseFloat(newDepositAmountString);
 
   //step-3: get the current deposit total
   //for non-input use innerText to get ot set the data. Warning 😡😡 don't use .value
   const depositTotalElement = document.getElementById('depositTotal');
-  const depositTotal = depositTotalElement.innerText;
-  depositTotalElement.innerText = depositAmount;
+  const previousDepositTotalString = depositTotalElement.innerText;
+  const previousDepositAmount = parseFloat(previousDepositTotalString);
+
+  //step-4: add numbers to set the total deposit
+  const currentDepositTotal = previousDepositAmount + newDepositAmount;
+  //set the deposit total
+  depositTotalElement.innerText = currentDepositTotal;
+
+  //step-5: get balance total
+  const balanceTotalElement = document.getElementById('balanceTotal');
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  //step-6: calculate curent total balance
+  const currentBalancetotal = previousBalanceTotal + newDepositAmount;
+  //set the balance total
+  balanceTotalElement.innerText = currentBalancetotal;
 
   //step-7: clear the deposit field
   depositField.value = '';
